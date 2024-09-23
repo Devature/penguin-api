ddc73690085e7f16838bb9650e9ecdbffefba99f
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-09-23 17:42:19 -0400

    initial commit

---
+++ src/test/java/com/devature/penguin_api/PenguinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devature.penguin_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenguinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
