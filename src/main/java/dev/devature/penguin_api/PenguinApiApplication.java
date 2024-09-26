45f9c1c67fee47b8a8462eba04ee4776ecb6ef87
Tony <allenr64y@ymail.com>
2024-09-26 14:45:15 -0500

    Changing working package from com to dev to reflect the domain change.

--- src/main/java/dev/devature/penguin_api/PenguinApiApplication.java
+++ src/main/java/dev/devature/penguin_api/PenguinApiApplication.java
@@ -1,4 +1,4 @@
-package com.devature.penguin_api;
+package dev.devature.penguin_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
