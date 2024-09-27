c47c2c39537dbc4a17f1a994df985279dce3d51f
Kyle Rego <regoky@outlook.com>
2024-09-27 13:07:17 -0400

    Add a failing test

---
+++ src/test/java/dev/devature/penguin_api/FailingTest.java
@@ -0,0 +1,13 @@
+package dev.devature.penguin_api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FailingTest {
+    
+    @Test
+    void FailingTest1() {
+        assertEquals(2, 3);
+    }
+}
