0df54f2072080566d5aebb2e8bffa15659ce42ec
Soot <r1tt3rjw@gmail.com>
2024-09-30 13:35:54 -0500

    fixed multiple imports in UserRepository

--- src/main/java/dev/devature/penguin_api/repository/UserRepository.java
+++ src/main/java/dev/devature/penguin_api/repository/UserRepository.java
@@ -3,13 +3,10 @@ package dev.devature.penguin_api.repository;
 import dev.devature.penguin_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import dev.devature.penguin_api.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
 
     @Query("SELECT u.password FROM User u WHERE u.email = :email")
