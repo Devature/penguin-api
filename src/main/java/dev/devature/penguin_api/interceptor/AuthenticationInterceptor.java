e54ae02f6c8db88e4f885a6a5aac32775f67a6fe
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-09-30 11:58:02 -0400

    adding functionality to issue and verify JWT tokens

---
+++ src/main/java/dev/devature/penguin_api/interceptor/AuthenticationInterceptor.java
@@ -0,0 +1,47 @@
+package dev.devature.penguin_api.interceptor;
+
+import dev.devature.penguin_api.service.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class AuthenticationInterceptor implements HandlerInterceptor {
+    private static final Set<String> OPEN_ENDPOINTS;
+
+    static {
+        OPEN_ENDPOINTS = new HashSet<>();
+        OPEN_ENDPOINTS.add("/api/v1/user/registration");
+        OPEN_ENDPOINTS.add("/api/v1/user/login");
+    }
+
+    private final JwtService jwtService;
+
+    @Autowired
+    public AuthenticationInterceptor(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getMethod().equals("OPTIONS")) return true;
+        if (OPEN_ENDPOINTS.contains(request.getRequestURI())) return true;
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            response.sendError(401, "Invalid authorization header");
+            return false;
+        }
+
+        String token = authorizationHeader.substring("Bearer ".length());
+        if (this.jwtService.verifyToken(token).isPresent()) return true;
+        else response.sendError(401, "Invalid authorization token");
+
+        return false;
+    }
+}
--- src/main/java/dev/devature/penguin_api/service/RegisterService.java
+++ src/main/java/dev/devature/penguin_api/service/RegisterService.java
@@ -2,7 +2,7 @@ package dev.devature.penguin_api.service;
 
 import dev.devature.penguin_api.config.SecurityConfig;
 import dev.devature.penguin_api.entity.User;
-import dev.devature.penguin_api.repository.RegisterRepository;
+import dev.devature.penguin_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,16 +12,17 @@ import java.util.regex.Pattern;
 @Service
 @Transactional
 public class RegisterService {
-    private final RegisterRepository registerRepository;
+    private static final String passwordRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[~`!@#$%^&*()\\-_+={}\\[\\]|;:<>,./?]).{8,}$";
+    private static final String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+    private final UserRepository userRepository;
     private final SecurityConfig securityConfig;
-    private final String passwordRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[~`!@#$%^&*()\\-_+={}\\[\\]|;:<>,./?]).{8,}$";
-    private final String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     private final Pattern passPattern;
     private final Pattern emailPattern;
 
     @Autowired
-    public RegisterService(RegisterRepository registerRepository, SecurityConfig securityConfig) {
-        this.registerRepository = registerRepository;
+    public RegisterService(UserRepository userRepository, SecurityConfig securityConfig) {
+        this.userRepository = userRepository;
         this.securityConfig = securityConfig;
 
         passPattern = Pattern.compile(passwordRegex);
@@ -50,7 +51,7 @@ public class RegisterService {
         String hashedPassword = securityConfig.passwordEncoder().encode(saltedPassword);
         user.setPassword(hashedPassword);
 
-        return registerRepository.save(user);
+        return userRepository.save(user);
     }
 
     /**
@@ -78,7 +79,7 @@ public class RegisterService {
      * @return Return {@code True} if email does not available or {@code False} if the email is unavailable.
      */
     public boolean checkEmailAvailable(String email){
-        User user = registerRepository.findByEmail(email);
+        User user = userRepository.findByEmail(email);
         return user == null;
     }
 }
