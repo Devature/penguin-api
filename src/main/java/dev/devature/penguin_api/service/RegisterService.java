51dfa9d85131a6cd534e0e5f257e2f1bff17aa61
Soot <r1tt3rjw@gmail.com>
2024-10-01 11:33:33 -0500

    changed RegisterService and RegisterServiceTest back to avoid merge conflicts. Tony should be the one to change these as he wrote them and knows more about them.

--- src/main/java/dev/devature/penguin_api/service/RegisterService.java
+++ src/main/java/dev/devature/penguin_api/service/RegisterService.java
@@ -3,52 +3,77 @@ package dev.devature.penguin_api.service;
 import dev.devature.penguin_api.config.SecurityConfig;
 import dev.devature.penguin_api.entity.User;
 import dev.devature.penguin_api.repository.UserRepository;
-import dev.devature.penguin_api.utils.EmailPasswordValidationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.regex.Pattern;
+
 @Service
 @Transactional
 public class RegisterService {
+    private static final String passwordRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[~`!@#$%^&*()\\-_+={}\\[\\]|;:<>,./?]).{8,}$";
+    private static final String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
     private final UserRepository userRepository;
     private final SecurityConfig securityConfig;
+    private final Pattern passPattern;
+    private final Pattern emailPattern;
 
     @Autowired
     public RegisterService(UserRepository userRepository, SecurityConfig securityConfig) {
         this.userRepository = userRepository;
         this.securityConfig = securityConfig;
+
+        passPattern = Pattern.compile(passwordRegex);
+        emailPattern = Pattern.compile(emailRegex);
     }
 
     /**
      * @param user Take in a {@code User} object to be processed.
-     * @return User {@code User} object if the account was successfully added or
+     * @return User {@code User} object if the account was successfully add or
      * {@code null} if it failed to add or meet requirements.
      */
-    public User registerUser(User user) {
-        boolean isEmailValid = EmailPasswordValidationUtils.isValidEmail(user.getEmail());
-        boolean isPasswordValid = EmailPasswordValidationUtils.isValidPassword(user.getPassword());
+    public User registerUser(User user){
+        boolean isEmailValid = checkEmailRequirements(user.getEmail());
+        boolean isPasswordValid = checkPasswordRequirements(user.getPassword());
         boolean isEmailAvailable = checkEmailAvailable(user.getEmail());
 
-        if (!isEmailValid || !isPasswordValid || !isEmailAvailable) {
+        if(!isEmailValid || !isPasswordValid || !isEmailAvailable){
             return null;
         }
 
-        String salt = securityConfig.generateNewSalt();
-        user.setSalt(salt);
-
-        String saltedPassword = user.getSalt() + user.getPassword();
-        String hashedPassword = securityConfig.passwordEncoder().encode(saltedPassword);
+        String hashedPassword = securityConfig.passwordEncoder().encode(user.getPassword());
         user.setPassword(hashedPassword);
 
         return userRepository.save(user);
     }
 
     /**
-     * @param email Take in a {@code String} to check if the email already exists in the database.
-     * @return Return {@code True} if email is not available or {@code False} if the email is available.
+     * This is based on the RFC 5322 requirements for email.
+     * @param email Takes in a  {@code String} email to be processed.
+     * @return A {@code True} if the password meets requirements or {@code False} if it fails to
+     * meet the requirements.
      */
-    public boolean checkEmailAvailable(String email) {
+    public boolean checkEmailRequirements(String email){
+        return emailPattern.matcher(email).matches();
+    }
+
+    /**
+     * This is based on 1 uppercase, 1 lowercase, 1 special, 1 number, and 8 characters or greater.
+     * @param password Take in {@code String} object password.
+     * @return A {@code True} if the password meets requirements or {@code False} if it fails to
+     * meet the requirements.
+     */
+    public boolean checkPasswordRequirements(String password){
+        return passPattern.matcher(password).matches();
+    }
+
+    /**
+     * @param email Take in a {@code String} to check if email already exist in database.
+     * @return Return {@code True} if email does not available or {@code False} if the email is unavailable.
+     */
+    public boolean checkEmailAvailable(String email){
         User user = userRepository.findByEmail(email);
         return user == null;
     }
