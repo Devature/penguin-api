947357c8fa139b3b051c854732e3031f058edfbd
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-11-19 01:14:43 -0500

    Refactored email tests to provide more information when test cases fail.

--- src/test/java/dev/devature/penguin_api/service/EmailPasswordValidationUtilsTest.java
+++ src/test/java/dev/devature/penguin_api/service/EmailPasswordValidationUtilsTest.java
@@ -3,37 +3,37 @@ package dev.devature.penguin_api.service;
 import dev.devature.penguin_api.utils.EmailPasswordValidationUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
 public class EmailPasswordValidationUtilsTest {
-    @Test
-    public void emailRequirementTest(){
-        String[] email = {"normal@example.com", "first.last@example.com", "user+tag@example.com",
-                "\"quoted@local\"@example.com", "special!#$%&'*+-/=?^_`{|}~@example.com", "plainaddress@.com",
-                "user@com", "user..name@example.com", "user@-example.com", "user@exa(mple).com"};
-        boolean[] expectedResult = {true, true, true, true, true, false, false, false, false, false};
-        boolean[] actualResult = new boolean[10];
-
-        for(int i = 0; i < email.length; ++i){
-            actualResult[i] = EmailPasswordValidationUtils.isValidEmail(email[i]);
-        }
-
-        Assertions.assertArrayEquals(expectedResult, actualResult,
-                "Expected Boolean True, True, True, True, True, False, False, False, False, False - " +
-                        "Actual Boolean was: " + Arrays.toString(actualResult));
+    @ParameterizedTest
+    @ValueSource(strings = { "normal@example.com", "first.last@example.com", "user+tag@example.com",
+            "\"quoted@local\"@example.com", "special!#$%&'*+-/=?^_`{|}~@example.com" })
+    public void validEmailRequirementTest(String email) {
+        Assertions.assertTrue(EmailPasswordValidationUtils.isValidEmail(email),
+                String.format("isValidEmail function should return true for valid email %s.", email));
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = { "plainaddress@.com", "user@com", "user..name@example.com", "user@-example.com",
+            "user@exa(mple).com" })
+    public void invalidEmailRequirementTest(String email) {
+        Assertions.assertFalse(EmailPasswordValidationUtils.isValidEmail(email),
+                String.format("isValidEmail function should return false for invalid email %s.", email));
+    }
 
     @Test
     public void passwordRequirementTest() {
         // Given
-        String[] password = {"1234", "Test_Password12", "TestPassword12", "Test_Password"};
-        boolean[] expectedResult = {false, true, false, false};
+        String[] password = { "1234", "Test_Password12", "TestPassword12", "Test_Password" };
+        boolean[] expectedResult = { false, true, false, false };
         boolean[] actualResult = new boolean[4];
 
         // When
-        for(int i = 0; i < password.length; ++i){
+        for (int i = 0; i < password.length; ++i) {
             actualResult[i] = EmailPasswordValidationUtils.isValidPassword(password[i]);
         }
 
