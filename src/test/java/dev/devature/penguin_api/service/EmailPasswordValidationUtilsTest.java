508a2a1caacabb6d4264a90d4994846b881e5e15
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-11-19 01:29:32 -0500

    Refactored password tests to provide more information when test cases fail.

--- src/test/java/dev/devature/penguin_api/service/EmailPasswordValidationUtilsTest.java
+++ src/test/java/dev/devature/penguin_api/service/EmailPasswordValidationUtilsTest.java
@@ -2,11 +2,8 @@ package dev.devature.penguin_api.service;
 
 import dev.devature.penguin_api.utils.EmailPasswordValidationUtils;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.Arrays;
 
 public class EmailPasswordValidationUtilsTest {
     @ParameterizedTest
@@ -25,21 +22,17 @@ public class EmailPasswordValidationUtilsTest {
                 String.format("isValidEmail function should return false for invalid email %s.", email));
     }
 
-    @Test
-    public void passwordRequirementTest() {
-        // Given
-        String[] password = { "1234", "Test_Password12", "TestPassword12", "Test_Password" };
-        boolean[] expectedResult = { false, true, false, false };
-        boolean[] actualResult = new boolean[4];
+    @ParameterizedTest
+    @ValueSource(strings = { "Test_Password12" })
+    public void validPasswordRequirementTest(String password) {
+        Assertions.assertTrue(EmailPasswordValidationUtils.isValidPassword(password),
+                String.format("isValidPassword should return true for valid password %s.", password));
+    }
 
-        // When
-        for (int i = 0; i < password.length; ++i) {
-            actualResult[i] = EmailPasswordValidationUtils.isValidPassword(password[i]);
-        }
-
-        // Then
-        Assertions.assertArrayEquals(expectedResult, actualResult,
-                "Expected Boolean False, True, False, False - Actual Boolean was: " +
-                        Arrays.toString(actualResult));
+    @ParameterizedTest
+    @ValueSource(strings = { "1234", "TestPassword12", "Test_Password" })
+    public void invalidPasswordRequirementTest(String password) {
+        Assertions.assertFalse(EmailPasswordValidationUtils.isValidPassword(password),
+                String.format("isValidPassword should return false for invalid password %s.", password));
     }
 }
